fc37885d9ff4c5a9be84a00430ce343b38b80fc0
Narendarkatta <k.narendarreddy111@gmail.com>
2022-04-11 11:01:11 -0500

    changes in controller and pug file

--- app.js
+++ app.js
@@ -4,13 +4,43 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const maogoose = require('mongoose')
+var Costume = require('./models/costume');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var computerRouter= require('./routes/computer');
-var addmodsRouter= require('./routes/addmods');
-var selctorRouter= require('./routes/selector');
+var computerRouter = require('./routes/computer');
+var addmodsRouter = require('./routes/addmods');
+var selctorRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
 
 var app = express();
+
+
+const dbURI = 'mongodb+srv://demo:demo@cluster0.wuvl3.mongodb.net/learnMongo?retryWrites=true&w=majority';
+
+maogoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('Connection to DB succeeded'))
+  .catch((err) => console.log(err));
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  // await Costume.deleteMany();
+  let instance1 = new
+    Costume({
+      costume_type: "ghost", size: 'large',
+      cost: 25.4
+    });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,14 +57,15 @@ app.use('/users', usersRouter);
 app.use('/computer', computerRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selctorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,3 +76,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
