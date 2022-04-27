f54590c7286350c78fae17b3906c83d6cebb6a9e
Narendarkatta <k.narendarreddy111@gmail.com>
2022-04-27 11:06:53 -0500

    adding secure data

--- routes/laptops.js
+++ routes/laptops.js
@@ -7,16 +7,22 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.render('laptops', { title: 'Search Results' });
 // });
-
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
 /* GET Laptops */
 router.get('/', laptop_controller.laptop_view_all_Page );
 /* GET detail laptop page */
 router.get('/detail', laptop_controller.laptop_view_one_Page);
 /* GET create laptop page */
-router.get('/create', laptop_controller.laptop_create_Page);
+router.get('/create',secured, laptop_controller.laptop_create_Page);
 /* GET create update page */
-router.get('/update', laptop_controller.laptop_update_Page);
+router.get('/update',secured, laptop_controller.laptop_update_Page);
 /* GET delete laptop page */
-router.get('/delete', laptop_controller.laptop_delete_Page);
+router.get('/delete',secured, laptop_controller.laptop_delete_Page);
 
 module.exports = router;
